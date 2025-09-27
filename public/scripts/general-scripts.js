16ca467a9b0c18f60db9556deceaef2748595c7d
Isaí Piedra <piedraisai24@gmail.com>
2025-09-27 17:10:27 -0600

    Diseño de la pantalla de creación de nuevo repositorio.

---
+++ public/scripts/general-scripts.js
@@ -0,0 +1,40 @@
+/* ---------- MESSAGE BEHAVIORS ---------- */
+const message_container = document.querySelector('.message_container');
+const close_message_btn = document.querySelector('#close_message_btn');
+
+close_message_btn.addEventListener('click', () => {
+    message_container.style = 'display:none;';
+});
+
+/* -------- SIDE BARS BEHAVIORS --------- */
+
+const tooltip_left = document.querySelector('#tooltip_left');
+const tooltip_right = document.querySelector('#tooltip_right');
+const tooltip_right_notification = document.querySelector('#tooltip_right_notification');
+
+const user_info_menu = document.querySelector('#user_info_menu');
+const menu_opener = document.querySelector('#menu_opener');
+const user_inbox_menu = document.querySelector('#user_inbox_menu');
+
+const user_info = document.querySelector('#user_info');
+const side_inbox = document.querySelector('#side_inbox');
+
+tooltip_left.addEventListener('click', () => {
+    side_inbox.style = 'display:none; transition:ease 0.2s';
+    user_inbox_menu.style = 'visibility:visible';
+});
+
+user_inbox_menu.addEventListener('click', () => {
+    side_inbox.style = 'display:flex';
+    user_inbox_menu.style = 'visibility:hidden';
+});
+
+tooltip_right.addEventListener('click', () => {
+    user_info.style = 'display:none; transition:ease 0.2s';
+    user_info_menu.style = 'visibility:visible';
+});
+
+menu_opener.addEventListener('click', () => {
+    user_info.style = 'display:flex';
+    user_info_menu.style = 'visibility:hidden';
+});
--- public/scripts/repository-info-scripts.js
+++ public/scripts/repository-info-scripts.js
@@ -1,40 +1,5 @@
-/* --------- REPOSITORY INFO BEHAVIOURS --------- */
+/* --------- RATING STARS BEHAVIORS --------- */
 
-/* Menu behaviors */
-const tooltip_right = document.querySelector('#tooltip_right');
-
-const user_info_menu = document.querySelector('#user_info_menu');
-const menu_opener = document.querySelector('#menu_opener');
-const user_info = document.querySelector('#user_info');
-
-const user_chat_menu = document.querySelector('#user_chat_menu');
-const chat_opener = document.querySelector('#chat_opener');
-const repository_discussion_right = document.querySelector('#repository_discussion_right');
-
-let chat_space_active = false;
-
-tooltip_right.addEventListener('click', () => {
-    user_info.style = 'display:none; transition:ease 0.2s';
-    user_info_menu.style = 'visibility:visible';
-});
-
-menu_opener.addEventListener('click', () => {
-    user_info.style = 'display:flex';
-    user_info_menu.style = 'visibility:hidden';
-});
-
-chat_opener.addEventListener('click', () => {
-    if (chat_space_active) {
-        repository_discussion_right.style = 'display:none';
-        chat_space_active = false;
-    } else {
-        repository_discussion_right.style = 'display:block';
-        chat_space_active = true;
-    }
-});
-
-
-/* Rating stars behaviors */
 const stars_count = document.querySelector('#stars_count');
 const stars = stars_count.children;
 
